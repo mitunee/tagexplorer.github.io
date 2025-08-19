deccb16f959990f966b406848a7f12d94c90f40c
mitunee <mitune.e.music@gmail.com>
2025-08-19 21:00:55 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tagexplorer.github.io/',
   plugins: [
     // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
     tanstackRouter({
